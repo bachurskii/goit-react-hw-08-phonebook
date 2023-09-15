101359606a570aca727e76c23191b06403ac9713
Georgen1 <123319264+bachurskii@users.noreply.github.com>
2023-09-15 10:46:50 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,9 +24,24 @@ export const App = () => {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="home"></Navigate>} />
+          <Route
+            index
+            element={
+              <>
+                <Navigate to="home" />
+                <HomePage />
+              </>
+            }
+          />
 
-          <Route path="home" element={<HomePage />} />
+          <Route
+            path="home"
+            element={
+              <>
+                <HomePage />
+              </>
+            }
+          />
           <Route
             path="register"
             element={
